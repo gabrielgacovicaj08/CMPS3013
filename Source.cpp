9ab21e84593d4695c390fa1fbbe026b72bc5bf7d
Gabriel Gacovicaj <gabriel.gacovicaj@gmail.com>
2025-01-27 21:10:42 +0000

    first adv structure class

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+class Heap{
+    int *heap;
+    int bum;
+    
+    void bubble_up(int i){
+        int parent = i/2;
+        while(heap[parent] > heap[i]){
+            swap(parent, i);
+            i = parent;
+            parent = i/2;
+            if(!parent) {
+                break
+            }
+        }
+    }
+
+    void swap(int i, int j){
+        int temp = heap[i];
+        heap[i] = heap[j];
+
+    }
+
+    public:
+    Heap(){
+        heap = new int[100];
+        bum = 1;
+
+    }
+
+    int pop(){return 0;}
+    void push(int x){
+        heap[bum] = x;
+        bum++;
+        bubble_up(bum - 1);
+    }
+}
 int main() {
-    cout << "Hello, World!" << endl;
+    
     return 0;
 }
